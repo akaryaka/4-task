6f820247ef0683b35c86aaa28bb1a0a31a404586
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-04 23:17:29 +0600

    ref init component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { Slider } from './components/slider/index..ts'
-import { Panel } from './components/panel.ts';
+// import { Slider } from './components/slider/index..ts'
 import './style.css'
+import { Panel } from './components/panel.ts';
+import { SliderView } from './components/slider/SliderView.ts';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const slider = new Slider(50, 75, 1);
+  // const slider = new Slider(50, 75, 1);
+  const sliderView = new SliderView();
   const panel = new Panel();
 })
