d41908a868f4c2034a14fd04a3fa5b0803474f38
Andrey <akariaka@yandex.ru>
2026-07-13 17:54:56 +0600

    edit import panel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // import { Slider } from './components/slider/index..ts'
 import './style.css'
-import { Panel } from './components/panel.ts';
+import { Panel } from './components/panel/panel.ts';
 import { SliderView } from './components/slider/SliderView.ts';
 
 document.addEventListener('DOMContentLoaded', function() {
